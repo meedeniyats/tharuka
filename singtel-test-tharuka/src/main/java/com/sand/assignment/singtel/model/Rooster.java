a279ee6cb9633c6fac89b9d615a39e5496f3ff9e
meedeniyats <meedeniyats@gmail.com>
2020-01-13 01:28:27 +0800

    Modified Rooster to support multiple languages

--- singtel-test-tharuka/src/main/java/com/sand/assignment/singtel/model/Rooster.java
+++ singtel-test-tharuka/src/main/java/com/sand/assignment/singtel/model/Rooster.java
@@ -1,10 +1,50 @@
 package com.sand.assignment.singtel.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Rooster extends Chicken {
 	
+	protected static Map<String, String> roosterDisctionary;
+	
+	protected String sound; 
+	
+	static {
+		roosterDisctionary = new HashMap<String, String>();
+	    roosterDisctionary.put("English", "Cock-a-doodle-doo");
+	    roosterDisctionary.put("Danish", "kykyliky");
+	    roosterDisctionary.put("Dutch", "kukeleku");
+	    roosterDisctionary.put("Finnish", "kukko kiekuu");
+	    roosterDisctionary.put("French", "cocorico");
+	    roosterDisctionary.put("German", "kikeriki");
+	    roosterDisctionary.put("Greek", "kikiriki");
+	    roosterDisctionary.put("Hebrew", "coo-koo-ri-koo");
+	    roosterDisctionary.put("Hungarian", "kukuriku");
+	    roosterDisctionary.put("Italian", "chicchirichi");
+	    roosterDisctionary.put("Japanese", "ko-ke-kok-ko-o");
+	    roosterDisctionary.put("Portuguese", "cucurucu");
+	    roosterDisctionary.put("Russian", "kukareku");
+	    roosterDisctionary.put("Swedish", "kuckeliku");
+	    roosterDisctionary.put("Turkish", "kuk-kurri-kuuu");
+	    roosterDisctionary.put("Urdu", "kuklooku");
+	}
+	
+	public Rooster(String language) {
+		super();
+		this.sound = roosterDisctionary.get(language);
+	}
+	
+	public Rooster() {
+		this.sound = roosterDisctionary.get("English");
+	}
+
+
+
+
+
 	@Override
 	public void sing() {
-		System.out.println("Cock-a-doodle-doo");
+		System.out.println(this.sound);
 	}
 
 }
